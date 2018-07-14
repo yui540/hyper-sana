2679ae1b2578bbcc12e6e72981888dda4bb346f9
yui540 <tabletennis540@gmail.com>
2018-07-14 21:48:32 +0900

    add: 画面に名取さなのイラストが表示されるようにスタイルを当てる

---
+++ src/stylesheets/term.js
@@ -0,0 +1,20 @@
+import illust from '../images/sana.svg'
+
+export default ({illust: show, opacity}) => {
+  return `
+    .terms_terms:before {
+      content: "";
+      display: ${show ? 'block' : 'none'};
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-image: url(${illust});
+      background-size: auto 75%;
+      background-position: right bottom;
+      background-repeat: no-repeat;
+      opacity: ${opacity};
+    }
+  `
+}
