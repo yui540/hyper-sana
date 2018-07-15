23518c666422c1ed8684999ec2d958bc12f078d4
yui540 <tabletennis540@gmail.com>
2018-07-15 13:32:08 +0900

    update: close-tabミドルウェアを適用

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,14 @@
 import sessionAddData from './session-add-data'
 import uiCommandExec from './ui-command-exec'
+import closeTab from './close-tab'
 
 export default (store, next, action) => {
   if ('SESSION_ADD_DATA' === action.type) {
     sessionAddData(store, next, action)
   } else if ('UI_COMMAND_EXEC' === action.type) {
     uiCommandExec(store, next, action)
+  } else if ('CLOSE_TAB' === action.type) {
+    closeTab(store, next, action)
   } else {
     next(action)
   }
