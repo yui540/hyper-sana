4916a2b94746b10fe1e2bdd9ac298325ba7aa8da
yui540 <tabletennis540@gmail.com>
2018-07-15 13:36:26 +0900

    update: initミドルウェアを適用

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,12 @@
+import init from './init'
 import sessionAddData from './session-add-data'
 import uiCommandExec from './ui-command-exec'
 import closeTab from './close-tab'
 
 export default (store, next, action) => {
-  if ('SESSION_ADD_DATA' === action.type) {
+  if ('INIT' === action.type) {
+    init(store, next, action)
+  } else if ('SESSION_ADD_DATA' === action.type) {
     sessionAddData(store, next, action)
   } else if ('UI_COMMAND_EXEC' === action.type) {
     uiCommandExec(store, next, action)
