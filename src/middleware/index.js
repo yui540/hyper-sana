68349148b81741561660e273bdd287a9b7632ba6
yui540 <tabletennis540@gmail.com>
2018-07-15 13:25:32 +0900

    update: ui-command-execミドルウェアを適用

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,11 @@
-import sessionAddData from './session-add-data.js'
+import sessionAddData from './session-add-data'
+import uiCommandExec from './ui-command-exec'
 
 export default (store, next, action) => {
   if ('SESSION_ADD_DATA' === action.type) {
     sessionAddData(store, next, action)
+  } else if ('UI_COMMAND_EXEC' === action.type) {
+    uiCommandExec(store, next, action)
   } else {
     next(action)
   }
